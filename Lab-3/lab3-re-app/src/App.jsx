9171942e9c9afacfde479fbe064afa43622f4ea4
jbjor <43211199+jbjor@users.noreply.github.com>
2025-11-17 15:58:02 +0200

    new stuff n changes
    removed redundant components

--- Lab-3/lab3-re-app/src/App.jsx
+++ Lab-3/lab3-re-app/src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import SimpleClassComponent from './components/SimpleClassComponent'
 import SimpleFunctionalComponent from './components/SimpleFunctionalComponent'
 import { Student, Teacher } from './components/helper'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 
 function App() {
@@ -12,9 +14,14 @@ function App() {
 
   return (
     <>
-     <SimpleClassComponent/>
-     <SimpleFunctionalComponent student={Student} teacher={Teacher} />
+    <BrowserRouter>
+     <Navbar/>
 
+     <Routes>
+      <Route path='/' element={<SimpleClassComponent/>}/>
+      <Route path='/functional' element={<SimpleFunctionalComponent student={Student} teacher={Teacher} />}/>
+     </Routes>
+    </BrowserRouter>
     </>
   )
 }
